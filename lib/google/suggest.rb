763783b43ef1b68675f27c2198fb39c5f036a4ba
Samuel Molnar <molnar.samuel@gmail.com>
2015-03-06 16:28:16 +0100

    Fix encoding in Google suggestions

--- lib/google/suggest.rb
+++ lib/google/suggest.rb
@@ -1,11 +1,11 @@
 module Google
   class Suggest
     def self.for(term, downloader: Scout::Downloader, **options)
-      url     = URI.encode("http://google.com/complete/search?q=#{term}&client=chrome")
+      url     = "http://google.com/complete/search?q=#{term}&client=chrome"
       content = downloader.download(url)
       data    = JSON.parse(content)
 
-      data[1]
+      data[1].map { |suggestion| suggestion.encode('UTF-8', 'WINDOWS-1250', invalid: :replace, undef: :replace, replace: '?') }
     end
   end
 end
