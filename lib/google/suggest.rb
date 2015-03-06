b81d2232ff1f0cb55b49e1fd1bd52c43e70005d6
Samuel Molnar <molnar.samuel@gmail.com>
2015-03-06 17:02:43 +0100

    Use only queries

--- lib/google/suggest.rb
+++ lib/google/suggest.rb
@@ -4,8 +4,13 @@ module Google
       url     = "http://google.com/complete/search?q=#{term}&client=chrome"
       content = downloader.download(url)
       data    = JSON.parse(content)
+      types   = data[4]['google:suggesttype']
 
-      data[1].map { |suggestion| suggestion.encode('UTF-8', 'WINDOWS-1250', invalid: :replace, undef: :replace, replace: '?') }
+      data[1].each_with_index.map { |suggestion, index|
+        next unless types[index] == 'QUERY'
+
+        suggestion.encode('UTF-8', 'WINDOWS-1252', invalid: :replace, undef: :replace, replace: '?')
+      }.compact
     end
   end
 end
--- spec/lib/google/suggest_spec.rb
+++ spec/lib/google/suggest_spec.rb
@@ -3,15 +3,30 @@ require 'spec_helper'
 describe Google::Suggest do
   describe '.for' do
     it 'returns suggestion for given term' do
-      downloader  = double(:downloader)
+      downloader = double(:downloader)
 
-      downloader.stub(:download).with('http://google.com/complete/search?q=googl&client=chrome') { fixture('google/suggest.json').read }
+      downloader.stub(:download).with('http://google.com/complete/search?q=g&client=chrome') { fixture('google/suggest.json').read }
 
-      suggestions = Google::Suggest.for('googl', downloader: downloader)
+      suggestions = Google::Suggest.for('g', downloader: downloader)
 
       expect(suggestions.size).to eql(20)
       expect(suggestions[0]).to eql('google')
-      expect(suggestions[1]).to eql('google maps')
+      expect(suggestions[1]).to eql('gmail')
+    end
+
+    context 'when suggestion contain resources other than queries' do
+      it 'ommits them' do
+        downloader = double(:downloader)
+
+        downloader.stub(:download).with('http://google.com/complete/search?q=google&client=chrome') { fixture('google/suggest_with_navigation.json').read }
+
+        suggestions = Google::Suggest.for('google', downloader: downloader)
+
+        expect(suggestions.size).to eql(18)
+        expect(suggestions[0]).to eql('google maps')
+        expect(suggestions[1]).to eql('google drive')
+        expect(suggestions[2]).to eql('google docs')
+      end
     end
   end
 end
